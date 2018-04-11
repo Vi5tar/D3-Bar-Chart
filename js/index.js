2294b18a2fe017ce8d25a575549f79c5e4bba88f
Travis Swiers <tswiers85@gmail.com>
2018-04-11 16:17:33 -0600

    We have a Bar Chart!
    * Followed instructions from here:
    https://medium.freecodecamp.org/how-to-create-your-first-bar-chart-with-d3-js-a0e8ea2df386
    * Was able to get the fetched data to display!

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+//let myData = {};
+const svgWidth = 700;
+const svgHeight = 500;
+const topGDP = 19000;
+
+const svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+  .attr("class", "bar-chart");
+
 fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
   .then(function(response) {
     return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
+  }).then(function(myJson) {
+    //myData = myJson;
+    //console.log(myJson.data);
+    const barWidth = (svgWidth / myJson.data.length);
+
+    const barChart = svg.selectAll("rect")
+      .data(myJson.data)
+      .enter()
+      .append("rect")
+      .attr("y", function(d) {
+        //const proportionalHeight = d[1] / topGDP;
+        return svgHeight - (svgHeight * (d[1] / topGDP));
+      })
+      .attr("height", function(d) {
+        //const proportionalHeight = d[1] / topGDP;
+        return svgHeight * (d[1] / topGDP);
+      })
+      .attr("width", barWidth)
+      .attr("transform", function(d, i) {
+        const translate = [barWidth * i, 0];
+        return "translate("+ translate +")";
+      })
   });
